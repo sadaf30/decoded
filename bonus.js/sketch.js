1c34143c0d559663f3fef5c15a60c517af174d11
sadaf30 <sadaf.shaikh@hotmail.com>
2017-05-19 16:23:11 -0400

    Add files via upload

---
+++ bonus.js/sketch.js
@@ -0,0 +1,36 @@
+//sadaf and nicole pair 
+
+var value='powderblue';
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background('lightyellow');
+  strokeWeight(5);
+  
+  fill('pink');
+  rect(250, 250, 50, 50);//car bumper
+  fill('yellow');
+  ellipse(300,280,20,20);//headlights
+  fill(value);
+  rect(100, 200, 150, 100);// body
+  fill('lavender');
+  rect(130, 230, 20, 20);//window
+  rect(208, 230, 20, 20);//window
+  ellipse(130, 300, 30, 30);//tires
+  ellipse(208, 300, 30, 30);//tires
+}
+  function mouseClicked(){//to change color
+     if (value=='powderblue') 
+     {
+        value='lightcoral';
+     }
+     else{
+        value='powderblue';
+     }
+     
+  }
+
+  
